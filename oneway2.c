a9daf3b125a6bfc9846e418b90be839805324605
gycg <1765479220@qq.com>
2017-02-22 09:55:27 +0800

    oneway2

--- oneway2.c
+++ oneway2.c
@@ -4,11 +4,11 @@
 
 static const char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 static const int alphabetSize = sizeof(alphabet) - 1;
-//char* digest(char* mess, char* argv1);
+char* digest(char* mess, char* argv1);
 void bruteImpl(char* str, int index, int maxDepth, char* argv1, char* argv2);
 void bruteSequential(int maxLen, char* argv1, char* argv2);
 
-//unsigned char md_value[EVP_MAX_MD_SIZE];
+unsigned char md_value[EVP_MAX_MD_SIZE];
 
 void bruteImpl(char* str, int index, int maxDepth, char* argv1, char* argv2)
 {
@@ -16,55 +16,28 @@ void bruteImpl(char* str, int index, int maxDepth, char* argv1, char* argv2)
     {
         str[index] = alphabet[i];
         if (index == maxDepth - 1) //printf("%s\n", str);
-	{
-		//char* md_value;
-		//md_value = digest(str, argv1);
+	    {
+		    unsigned char *md_v;
+		    md_v = digest(str, argv1);
 		
-
-            EVP_MD_CTX *mdctx;
-    const EVP_MD *md;
-    unsigned static char md_value[EVP_MAX_MD_SIZE];
-    int md_len, i;
-    
-    OpenSSL_add_all_digests();
-    
-
-    md = EVP_get_digestbyname(argv1);
-
-        if(!md) {
-            printf("Unknown message digest %s\n", argv1);
-            exit(1);
-    }   
-
-    mdctx = EVP_MD_CTX_create();
-    EVP_DigestInit_ex(mdctx, md, NULL);
-
-
-
-    EVP_DigestUpdate(mdctx, str, strlen(str));
-    EVP_DigestFinal_ex(mdctx, md_value, &md_len);
-    EVP_MD_CTX_destroy(mdctx);
-    EVP_cleanup();
-    
-    char outword[7];
-    for(i = 0; i<3; i++){
-        sprintf(outword+i*2, "%02x", md_value[i]);
-    }
-	//	printf("%02x\n",md_value[1]);
-
-        int X = 1;
-		for(int j = 0; j < 6; ++j)
-		{
-    		if(outword[j] != argv2[j])
-			{	
-                X = 0;
-                //break;	
-	            //printf("%02x\n", outword[j]);
+            char outword[7];
+            for(int j = 0; j<3; j++){
+                sprintf(outword+j*2, "%02x", md_v[j]);
             }
-		}
-		if(X == 1)
-			printf("%s\n", str);
-	}
+
+            int X = 1;
+		    for(int k = 0; k < 6; ++k)
+		    {
+    		    if(outword[k] != argv2[k])
+			    {	
+                    X = 0;
+	                //printf("%02x\n", outword[k]);
+                    break;	
+                }
+		    }
+		    if(X == 1)
+			    printf("%s\n", str);
+	    }
         else bruteImpl(str, index + 1, maxDepth, argv1, argv2);
     }
 }
@@ -84,15 +57,13 @@ void bruteSequential(int maxLen, char* argv1, char* argv2)
 
 
 
-/*char* digest(char* mess, char* argv1)
+char* digest(char* mess, char* argv1)
 {
 	EVP_MD_CTX *mdctx;
 	const EVP_MD *md;
-	unsigned static char md_value[EVP_MAX_MD_SIZE];
 	int md_len, i;
 	
 	OpenSSL_add_all_digests();
-	
 
 	md = EVP_get_digestbyname(argv1);
 
@@ -111,7 +82,7 @@ void bruteSequential(int maxLen, char* argv1, char* argv2)
    	EVP_MD_CTX_destroy(mdctx);
 	EVP_cleanup();
 	return md_value;	
-}*/
+}
 
 int main(int argc, char *argv[])
 {
@@ -122,12 +93,7 @@ int main(int argc, char *argv[])
 	char* argv1 = argv[1];
 	char* argv2 = argv[2];
 		
-	bruteSequential(3, argv1, argv2);
+	bruteSequential(2, argv1, argv2);
 
 	exit(0);
 }
-
-	
-
-
-
